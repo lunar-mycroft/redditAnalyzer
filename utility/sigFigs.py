1af5fd07d9f6a5ce54226c1055a8c856ee7ef6dd
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-02-14 14:57:08 -0500

    Got markdown generation working

--- utility/sigFigs.py
+++ utility/sigFigs.py
@@ -1,4 +1,7 @@
 from math import log10,floor
 
 def sigFigs(number, numFigs):
-    return round(number, (numFigs-1)-int(log10(number)))
+    if abs(number)>1:
+        return round(number, (numFigs-1)-int(log10(number)))
+    else:
+        return round(number, numFigs-int(log10(number)))
