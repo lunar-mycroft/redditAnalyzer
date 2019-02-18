9f1262a46e341f05bcccc562a3f77a9bf2c569af
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-02-18 13:30:16 -0500

    Added the code to read comments and posts

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import json
 
 from praw import Reddit
 
+from utility.getWords import postWords, commentWords
+
 config = {}
 with open('config.json', 'r') as configFile:
     config=json.load(configFile)
+wordDict = {}
+with open('words.json', 'r') as wordFile:
+    wordDict = json.load(wordFile)
+print("Finished loading dataset")
 
 reddit = Reddit(
     client_id = config['client_id'],
@@ -20,3 +26,26 @@ reddit = Reddit(
 )
 
 print("logged on as", reddit.user.me())
+
+user = reddit.redditor(config["username"])
+
+numPosts = 0
+numComments = 0
+
+for comment in user.comments.new(limit=None):
+    numComments += 1
+    for word, n in commentWords(comment).items():
+        if word in wordDict:
+            wordDict[word][0]+=n
+        else:
+            wordDict[word]=[n,0]
+print("Finished loading comments")
+
+for post in user.submissions.new(limit=None):
+    numPosts += 1
+    for word, n in postWords(post).items():
+        if word in wordDict:
+            wordDict[word][0]+=n
+        else:
+            wordDict[word]=[n,0]
+print("finished loading posts")
