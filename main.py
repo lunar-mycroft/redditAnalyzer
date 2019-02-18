870884a3df5c35c002dcdbdf5e73e251bee65c9c
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-02-18 13:22:06 -0500

    Added the milestone 1 code

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# Your code will go here
+import json
+
+from praw import Reddit
+
+config = {}
+with open('config.json', 'r') as configFile:
+    config=json.load(configFile)
+
+reddit = Reddit(
+    client_id = config['client_id'],
+    client_secret = config['client_secret'],
+    user_agent="{}:{}:{} (by {})".format(
+        config['user_agent']['platform'],
+        config['user_agent']['name'],
+        config['user_agent']['version'],
+        config['user_agent']['author']
+    ),
+    username = config["auth"]["username"],
+    password = config["auth"]["password"]
+)
+
+print("logged on as", reddit.user.me())
